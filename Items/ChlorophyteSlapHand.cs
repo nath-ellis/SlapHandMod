2dbf63d534784d356c82a6889ff4de5363d515a6
Nathan <nathanellis15782@protonmail.com>
2022-07-10 16:29:49 +0100

    Added Chlorophyte Slap Hand

---
+++ Items/ChlorophyteSlapHand.cs
@@ -0,0 +1,44 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace SlapHandMod.Items 
+{
+    public class ChlorophyteSlapHand : ModItem
+    {
+		public override void SetStaticDefaults()
+        {
+            
+        }
+
+		public override void SetDefaults()
+        {
+            Item.damage = 63;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 21;
+            Item.useAnimation = 21;
+            Item.useStyle = 1;
+            Item.knockBack = 20;
+            Item.value = 10000;
+            Item.rare = 7;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.crit = 15;
+
+            Item.shoot = ProjectileID.SporeCloud;
+            Item.shootSpeed = 8;
+        }
+
+		public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.ChlorophyteBar, 12);
+            recipe.AddIngredient(ItemID.SlapHand, 1);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.Register();
+        }
+    }
+}
