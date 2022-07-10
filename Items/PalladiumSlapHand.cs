17ccdeeb02beddb56c731bc152df9605dad20889
Nathan <nathanellis15782@protonmail.com>
2022-07-10 10:55:54 +0100

    Added the Palladium Slap Hand

---
+++ Items/PalladiumSlapHand.cs
@@ -0,0 +1,40 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SlapHandMod.Items 
+{
+    public class PalladiumSlapHand : ModItem
+    {
+		public override void SetStaticDefaults()
+        {
+            
+        }
+
+		public override void SetDefaults()
+        {
+            Item.damage = 41;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 21;
+			Item.useAnimation = 21;
+			Item.useStyle = 1;
+			Item.knockBack = 20;
+			Item.value = 10000;
+			Item.rare = 4;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+            Item.crit = 15;
+        }
+
+		public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.PalladiumBar, 10);
+			recipe.AddIngredient(ItemID.SlapHand, 1);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+        }
+    }
+}
