9b65c29cad1f708c99373e62770d9ddb234433d3
Nathan <nathanellis15782@protonmail.com>
2022-07-22 16:28:13 +0100

    Made the Meowmere Slap Hand a 100% drop rate from the Moon Lord

--- SlapHandMod.cs
+++ SlapHandMod.cs
@@ -1,11 +1,23 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
 
 namespace SlapHandMod
 {
     public class SlapHandMod : Mod
     {
 
+    }
+
+    public class ModGlobalNPC : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.MoonLordCore)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.MeowmereSlapHand>()));
+            }
+        }
     }
 }
